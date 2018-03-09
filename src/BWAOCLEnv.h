38fc04646a105fc42afc8b82dadab5866fc1c0c8
Di Wu <allwu@falcon-computing.com>
2018-03-08 17:56:14 -0800

    Add support to extract FPGA PAC from existing file, rather than a separate PAC file

--- src/BWAOCLEnv.h
+++ src/BWAOCLEnv.h
@@ -12,23 +12,35 @@
 #include <CL/opencl.h>
 
 #include "kflow/Queue.h"
+#include "config.h"
 #include "OpenCLEnv.h"
 #include "util.h"
 
+#define _set_pac(pac, l, c) ((pac)[(l)>>2] |= (c)<<((~(l)&3)<<1))
+#define _get_pac(pac, l) ((pac)[(l)>>2]>>((~(l)&3)<<1)&3)
+
 class BWAOCLEnv :public OpenCLEnv{
  public:
-  BWAOCLEnv(const char* bin_path, const char* pac_path, const char* kernel_name)
-    :OpenCLEnv(bin_path, kernel_name) {
-     FILE* pac_inp = fopen(pac_path, "rb");
-     int pac_size = 0;
-     fseek(pac_inp, 0, SEEK_END);
-     pac_size = ftell(pac_inp);
-     fseek(pac_inp, 0, SEEK_SET);
-     pac_size = (pac_size+3)/4;
+  BWAOCLEnv(
+      const char* bin_path, 
+      const char* pac_path, 
+      const char* kernel_name) :OpenCLEnv(bin_path, kernel_name) 
+  {
+     // get full pac array from pac
+     int64_t l_pac = aux->idx->bns->l_pac;
+     int64_t pac_size = (aux->idx->bns->l_pac * 2 + 3) / 4;
+     char *pac = (char*)calloc(pac_size, 1);
 
-     int *pac = (int*)malloc(sizeof(int)*pac_size);
-     fread(pac, sizeof(int), pac_size, pac_inp);
-     fclose(pac_inp);
+     memcpy(pac, aux->idx->pac, 
+            (aux->idx->bns->l_pac>>2) + 
+            (aux->idx->bns->l_pac&3) == 0? 0 : 1);
+
+     int64_t k = l_pac;
+     for (int64_t l = l_pac - 1; l >= 0; --l) {
+       _set_pac(pac, k, 3-_get_pac(pac, l));
+       k++;
+     }
+     DLOG(INFO) << "pac size = " << pac_size;
 
      int err = 0;
      command_ = clCreateCommandQueue(context_, device_id_, 0, &err);
@@ -41,15 +53,15 @@ class BWAOCLEnv :public OpenCLEnv{
      ext_d.flags = XCL_MEM_DDR_BANK3; ext_d.obj = 0; ext_d.param = 0;
 
      pac_input_a_ = clCreateBuffer(context_, CL_MEM_READ_ONLY | CL_MEM_EXT_PTR_XILINX,
-         sizeof(int) * pac_size, &ext_c, &err);
+         pac_size, &ext_c, &err);
      pac_input_b_ = clCreateBuffer(context_, CL_MEM_READ_ONLY | CL_MEM_EXT_PTR_XILINX,
-         sizeof(int) * pac_size, &ext_d, &err);
+         pac_size, &ext_d, &err);
      if (err != CL_SUCCESS) {
        throw std::runtime_error("Failed to create reference OpenCL buffer!");
      }
      cl_event event[2];
-     err = clEnqueueWriteBuffer(command_, pac_input_a_, CL_TRUE, 0, sizeof(int) * pac_size, pac, 0, NULL, &event[0]);
-     err = clEnqueueWriteBuffer(command_, pac_input_b_, CL_TRUE, 0, sizeof(int) * pac_size, pac, 0, NULL, &event[1]);
+     err = clEnqueueWriteBuffer(command_, pac_input_a_, CL_TRUE, 0, pac_size, pac, 0, NULL, &event[0]);
+     err = clEnqueueWriteBuffer(command_, pac_input_b_, CL_TRUE, 0, pac_size, pac, 0, NULL, &event[1]);
      clWaitForEvents(2, event);
      if (err != CL_SUCCESS) {
        throw std::runtime_error("Failed to write reference to DDR!");
--- src/config.h
+++ src/config.h
@@ -2,6 +2,8 @@
 #define CONFIG_H
 
 #include <gflags/gflags.h>
+#include <string>
+
 #include "bwa_wrapper.h"
 
 #define FPGA_RET_PARAM_NUM 5
@@ -16,13 +18,16 @@ DECLARE_int32(filter);
 DECLARE_int32(chunk_size);
 DECLARE_int32(max_fpga_thread);
 DECLARE_int32(max_num_records);
+DECLARE_int32(extra_thread);
 DECLARE_int32(output_flag);
 DECLARE_int32(stage_1_nt);
 DECLARE_int32(stage_2_nt);
 DECLARE_int32(stage_3_nt);
 DECLARE_int32(t);
 DECLARE_string(fpga_path);
+DECLARE_string(pac_path);
 DECLARE_string(output_dir);
+DECLARE_int32(output_nt);
 DECLARE_string(R);
 DECLARE_int32(max_batch_records);
 
