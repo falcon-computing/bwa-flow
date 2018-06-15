cd437540fc3710534b11119cea3c92115eb4a146
Junyi Qiu <junyiqiu@falcon-computing.com>
2018-06-15 12:17:21 -0700

    Init building with cmake

---
+++ cmake/FindXilinx.cmake
@@ -0,0 +1,7 @@
+add_custom_target(Xilinx)
+
+set(Xilinx_DIR "$ENV{XILINX_SDX}")
+message(STATUS "${Xilinx_DIR}")
+set(Xilinx_INCLUDE_DIRS "${Xilinx_DIR}/runtime/include/1_2")
+set(Xilinx_LIBRARY_DIRS "${Xilinx_DIR}/runtime/lib/x86_64")
+set(Xilinx_LIBRARIES "xilinxopencl")
