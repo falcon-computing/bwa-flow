74e329148920ce1237ec85bed79a3441c1c4f82c
Di Wu <allwu@cs.ucla.edu>
2018-09-17 12:15:11 -0700

    upgrade license module to v1.2 (#75)

--- cmake/FindFalconLM.cmake
+++ cmake/FindFalconLM.cmake
@@ -1,16 +1,16 @@
 ExternalProject_Add(falconlm-download
-    PREFIX "deps/falconlm"
-    URL https://s3.amazonaws.com/fcs-build-public/falcon-lic.tar.gz
-    URL_MD5 8f36e6cb08c7cb7a677d749bee1bfe6e
+    PREFIX "falconlm"
+    URL https://s3.amazonaws.com/fcs-build-public/falcon-lic-v1.2.tgz
+    URL_MD5 911e5526bbbb4b2967580173b59cc0ff 
     CONFIGURE_COMMAND ""
-    SOURCE_DIR "${CMAKE_BINARY_DIR}/deps/falconlm/install"
+    SOURCE_DIR "${CMAKE_BINARY_DIR}/falconlm/install"
     BUILD_COMMAND ""
     INSTALL_COMMAND "")
 
 add_custom_target(FalconLM)
 add_dependencies(FalconLM falconlm-download)
 
-set(FalconLM_DIR "${CMAKE_BINARY_DIR}/deps/falconlm/install")
+set(FalconLM_DIR "${CMAKE_BINARY_DIR}/falconlm/install")
 set(FalconLM_INCLUDE_DIRS "${FalconLM_DIR}/include")
 set(FalconLM_LIBRARY_DIRS "${FalconLM_DIR}/lib")
 set(FalconLM_LIBRARIES "falcon_license")
